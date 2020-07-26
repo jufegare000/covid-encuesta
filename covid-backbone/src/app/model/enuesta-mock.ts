8f5bfe9ab7dcb42ae3859359d5dd9252e29d2c5a
Juan Gallo <jufegare@gmail.com>
2020-07-26 02:41:10 -0500

    añadiendo librería de tslint

---
+++ covid-backbone/src/app/model/enuesta-mock.ts
@@ -0,0 +1,54 @@
+import { Encuesta } from './Encuesta';
+
+export const ENCUESTAS: Encuesta[] = [
+  {
+    ciudad: 'Medellin',
+    estratoSocial: '2',
+    ingresoPromedio: 4.3,
+    labora: false,
+    nivelEducacion: 'primaria',
+    nombrePersona: 'yeison',
+    sigueProtocolos: true,
+    sufreEstres: true
+  },
+  {
+    ciudad: 'Medellin',
+    estratoSocial: '2',
+    ingresoPromedio: 4.3,
+    labora: false,
+    nivelEducacion: 'primaria',
+    nombrePersona: 'yeison',
+    sigueProtocolos: true,
+    sufreEstres: true
+  },
+  {
+    ciudad: 'Medellin',
+    estratoSocial: '2',
+    ingresoPromedio: 4.3,
+    labora: false,
+    nivelEducacion: 'primaria',
+    nombrePersona: 'yeison',
+    sigueProtocolos: true,
+    sufreEstres: true
+  },
+  {
+    ciudad: 'Medellin',
+    estratoSocial: '2',
+    ingresoPromedio: 4.3,
+    labora: false,
+    nivelEducacion: 'primaria',
+    nombrePersona: 'yeison',
+    sigueProtocolos: true,
+    sufreEstres: true
+  },
+  {
+    ciudad: 'Medellin',
+    estratoSocial: '2',
+    ingresoPromedio: 4.3,
+    labora: false,
+    nivelEducacion: 'primaria',
+    nombrePersona: 'yeison',
+    sigueProtocolos: true,
+    sufreEstres: true
+  },
+];
