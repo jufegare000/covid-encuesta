19107eec5547c4d55b617f2afa500a206aa0cbe5
Juan Gallo <jufegare@gmail.com>
2020-07-26 13:56:03 -0500

    rutas agregadas

--- covid-backbone/src/app/app-routing.module.ts
+++ covid-backbone/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { EncuestasListComponent } from './encuestas-list/encuestas-list.component';
+import { EncuestaComponent } from './encuesta/encuesta.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-  //{path: 'encuentas', component: }
+  { path: 'encuesta', component: EncuestaComponent },
+  { path: 'encuestas-list', component: EncuestasListComponent }
 ];
 
 @NgModule({
